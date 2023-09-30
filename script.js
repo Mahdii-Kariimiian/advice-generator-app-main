aad95e7f6653bd3e5579708e2bb0df8fd63adeb2
Mahdi Karimian <mehdi.karimian.777@gmail.com>
2023-09-30 11:48:56 +0200

    add animation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const adviceContent = document.querySelector(".advice-content")
 const adId = document.querySelector(".ad-id")
 const svgSec = document.querySelector(".svg-sec")
 
+
 async function getAdvice (advice) {
     const response = await fetch(advice)
     const data = await response.json()
@@ -10,16 +11,20 @@ async function getAdvice (advice) {
 }
 
 function useAdvice (data) {
+    
     const slipData = data["slip"]
     adId.innerText = slipData["id"]
     adviceContent.innerText = '"' + slipData["advice"] + '"'
+    adviceContent.classList.add("pop-up")
 }
 
+// for Advice to show when the site opens
 getAdvice(url)
 
 svgSec.addEventListener('click' , ()=> {
-    const rand = Math.floor(Math.random() * 150)
+    let rand = Math.floor(Math.random() * 150)
     url = `https://api.adviceslip.com/advice/${rand}`
     getAdvice(url)
+    adviceContent.classList.remove("pop-up")
 })
 
